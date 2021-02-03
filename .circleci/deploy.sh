012a3c1dc4c24bbad62c7920eac50038ebb9b86e
Vlad <vadyalex@gmail.com>
2021-02-03 01:27:54 +0100

    [CircleCI, deploy] Add HTTPS redirect

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,20 +7,24 @@ TOKEN="$3"
 if [ -z "$(docker service ls | grep ordnerd)" ]; then
   echo "Service ordnerd does not exist! Create using ${TAG} image..";
 
-  docker service create                                                         \
-    --name 'ordnerd'                                                            \
-    --replicas 2                                                                \
-    --update-delay 10s                                                          \
-    --env PORT=5000                                                             \
-    --env WEBHOOK=${WEBHOOK}                                                    \
-    --env TOKEN=${TOKEN}                                                        \
-    --network traefik                                                           \
-    --label 'traefik.enable=true'                                               \
-    --label 'traefik.http.services.ordnerd.loadbalancer.server.port=5000'       \
-    --label 'traefik.http.routers.ordnerd.rule=Host(`ordnerd.app.vadyalex.me`)' \
-    --label 'traefik.http.routers.ordnerd.entrypoints=websecure'                \
-    --label 'traefik.http.routers.ordnerd.tls=true'                             \
-    --label 'traefik.http.routers.ordnerd.tls.certresolver=letsencrypt'         \
+  docker service create                                                             \
+    --name 'ordnerd'                                                                \
+    --replicas 2                                                                    \
+    --update-delay 10s                                                              \
+    --env PORT=5000                                                                 \
+    --env WEBHOOK=${WEBHOOK}                                                        \
+    --env TOKEN=${TOKEN}                                                            \
+    --network traefik                                                               \
+    --label 'traefik.enable=true'                                                   \
+    --label 'traefik.http.services.ordnerd.loadbalancer.server.port=5000'           \
+    --label 'traefik.http.routers.ordnerd.entrypoints=websecure'                    \
+    --label 'traefik.http.routers.ordnerd.rule=Host(`ordnerd.app.vadyalex.me`)'     \
+    --label 'traefik.http.routers.ordnerd.tls=true'                                 \
+    --label 'traefik.http.routers.ordnerd.tls.certresolver=letsencrypt'             \
+    --label 'traefik.http.middlewares.https_redirect.redirectscheme.scheme=https'   \
+    --label 'traefik.http.routers.ordnerd_web.entrypoints=web'                      \
+    --label 'traefik.http.routers.ordnerd_web.rule=Host(`ordnerd.app.vadyalex.me`)' \
+    --label 'traefik.http.routers.ordnerd_web.middlewares=https_redirect'           \
     vadyalex/ordnerd:${TAG}
 
    echo 'Done!';
